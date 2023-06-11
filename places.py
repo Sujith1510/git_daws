e3f6187f6f6c10a8fffe658d6ee0299e877bee5d
Sujith1510 <sujithat99@gmail.com>
2023-06-11 19:16:52 +0530

    added new file

---
+++ places.py
@@ -0,0 +1,7 @@
+place=("enter the country name:")
+if(place=="india"):
+    print("Tiger")
+elif(place=="australia"):
+    print("Kangaroo")
+else:
+    print("enter world test championship finals nations")
