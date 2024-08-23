e927fa49dc852eb51e3e2fb037121577f85f5b00
sujith1510 <sujithat99@gmail.com>
2024-08-23 18:52:13 +0530

    cleaning repo files

--- hungry.py
+++
@@ -1,7 +0,0 @@
-hungry=input("Are you hungry? ")
-if hungry=="yes":
-    print("eat chicken noodles or egg noodles")
-elif hungry=="no":
-    print("Drink water and go to sleep")
-else:
-    print("enter yes or no")
