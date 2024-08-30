e401570e755dab0a1ffef7c410cc3ce1442cad45
sujith1510 <sujithat99@gmail.com>
2024-08-30 15:58:17 +0530

    updating file

--- shell-practice/installation.sh
+++ shell-practice/installation.sh
@@ -7,18 +7,29 @@ N="\e[0m"
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
-then
-   echo -e "$R please use root access for installation...$N"
-   exit 1
-fi
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R please use root access for installation...$N"
+        exit 1
+    fi
+}
+
+VALIDATE(){
+  echo  "exit status: $1"
+}
+
+CHECK_ROOT 
 
 for package in $@
 do 
   dnf list installed $package
+  VALIDATE $?
   if [ $? -ne 0 ]
   then 
-     echo -e "$Y $package is not installed on the server...$N"
+     echo -e "$Y $package is not installed going to install $N"
+    #  dnf install $package -y
+    #  VALIDATE $?
   else
      echo -e "$G $package is installed already...$N"
   fi
