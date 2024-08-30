4ddc335e190772caedfd8c11982055d5faac4bba
sujith1510 <sujithat99@gmail.com>
2024-08-30 16:44:00 +0530

    updating file

--- shell-practice/installation.sh
+++ shell-practice/installation.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+LOGFOLDER="\var\log\shell-scripts"
+SCRIPT_NAME=$(echo $0 | cut -d . -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGFOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGFOLDER
+
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -10,7 +16,7 @@ USERID=$(id -u)
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-        echo -e "$R please use root access for installation...$N"
+        echo -e "$R please use root access for installation...$N " | tee -a $LOG_FILE
         exit 1
     fi
 }
@@ -18,9 +24,9 @@ CHECK_ROOT(){
 VALIDATE(){
   if [ $1 -ne 0 ]
   then 
-     echo -e "$2 installation is $R FAILED..$N"
+     echo -e "$2 installation is $R FAILED..$N" | tee -a $LOG_FILE
   else
-     echo -e "$2 installation is $G SUCCESSFULL..$N"
+     echo -e "$2 installation is $G SUCCESSFULL..$N" | tee -a $LOG_FILE
   fi
 }
 
@@ -28,13 +34,13 @@ CHECK_ROOT
 
 for package in $@
 do 
-  dnf list installed $package
+  dnf list installed $package &>> $LOG_FILE
   if [ $? -ne 0 ]
   then 
-     echo -e "$Y $package is not installed going to install $N"
-     dnf install $package -y
-     VALIDATE $? $package
+     echo -e "$Y $package is not installed going to install $N" | tee -a $LOG_FILE
+     dnf install $package -y &>> $LOG_FILE
+     VALIDATE $? $package 
   else
-     echo -e "$G $package is installed already...$N"
+     echo -e "$G $package is installed already...$N" | tee -a $LOG_FILE
   fi
 done
