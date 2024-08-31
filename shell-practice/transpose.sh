308bdc1816457753d25573b1c5ec3e3eb813a16d
sujith1510 <sujithat99@gmail.com>
2024-08-31 20:02:12 +0530

    updating file

--- shell-practice/transpose.sh
+++ shell-practice/transpose.sh
@@ -22,11 +22,9 @@ awk '
         if (NR == 1) {
             # For the first row, create the array
             transposed[i] = $i
-            echo "$i"
         } else {
             # For subsequent rows, append values with space
             transposed[i] = transposed[i] " " $i
-            echo "$transposed[i]"
         }
     }
 }
