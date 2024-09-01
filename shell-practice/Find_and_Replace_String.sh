cf70230fb02d99e3a9131a4d449e6c77d0e8185e
sujith1510 <sujithat99@gmail.com>
2024-09-01 19:23:07 +0530

    updating file

--- shell-practice/Find_and_Replace_String.sh
+++ shell-practice/Find_and_Replace_String.sh
@@ -25,7 +25,7 @@ fi
 # Find all .txt files in the specified directory and its subdirectories
 find "$DIRECTORY" -type f -name "*.txt" | while read -r F; do
   # Use sed to replace the search string with the replacement string in each file
-  if [ $FIND_STRING -eq $REPLACE_STRING ]
+  if [ $FIND_STRING == $REPLACE_STRING ]
   then     
     echo -e "Replaced the string in the files $R FAILED $N $Y Please enter the existing string $N"
   else
