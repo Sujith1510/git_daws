015318c806e521912303c40fb2715d69842fe95f
sujith1510 <sujithat99@gmail.com>
2024-09-01 18:46:01 +0530

    updating file

--- shell-practice/Find_and_Replace_String.sh
+++ shell-practice/Find_and_Replace_String.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Define the directory, search string, and replacement string
-DIRECTORY="/home/ec2-user/dir"
-FIND_STRING="hi"
-REPLACE_STRING="Hi"
+# DIRECTORY="/home/ec2-user/dir"
+# FIND_STRING="hi"
+# REPLACE_STRING="Hi"
 
 # Find all .txt files in the specified directory and its subdirectories
-find "$DIRECTORY" -type f -name "*.txt" | while read -r F; do
+find "$1" -type f -name "*.txt" | while read -r F; do
   # Use sed to replace the search string with the replacement string in each file
-  sed -i "s/$FIND_STRING/$REPLACE_STRING/g" "$F"
+  sed -i "s/$2/$3/g" "$F"
 done
 
